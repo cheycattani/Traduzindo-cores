bf4bf575fa2e48c10d26fa824e8b00a7060e6727
cheycattani <68974541+cheycattani@users.noreply.github.com>
2022-08-30 22:04:06 -0300

    Update traduzindo_cores.py

--- traduzindo_cores.py
+++ traduzindo_cores.py
@@ -2,12 +2,12 @@
 # Caso exista a cor no dicionário, imprima a tradução, caso contrário, imprima como "Esta cor não consta no disc."
 
 # Dicionário "cor"
-cor ={'vermelho':'red', 'rosa': 'pink', 'preto': 'black', 'azul': 'blue', 'roxo': 'purple', 'amarelo': 'yellow', 'laranja': 'orange', 'azul escuro': 'dark blue',
+cores ={'vermelho':'red', 'rosa': 'pink', 'preto': 'black', 'azul': 'blue', 'roxo': 'purple', 'amarelo': 'yellow', 'laranja': 'orange', 'azul escuro': 'dark blue',
       'verde': 'green', 'cinza': 'grey', 'branco': 'white'}
 
-question = input('Digite uma cor em portugês: ')
+cor = input('Digite uma cor em portugês: ')
 
 #O get() é um método usado para pegar o valor de uma dada chave em um dicionário se a chave estiver no dicionário,
 # caso ela não exista, o método retorna None ou o valor padrão passado por parâmetro.
 
-print('A tradução dessa cor é', cor.get(question, 'Esta cor não consta no meu discionário.'))
+print('A tradução dessa cor é', cores.get(cor, 'Esta cor não consta no meu discionário.'))
