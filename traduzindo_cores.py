da75a1d8370b21e076754e3af548d20853fd44ab
cheycattani <68974541+cheycattani@users.noreply.github.com>
2022-08-30 22:12:10 -0300

    Update traduzindo_cores.py

--- traduzindo_cores.py
+++ traduzindo_cores.py
@@ -5,7 +5,8 @@
 cores ={'vermelho':'red', 'rosa': 'pink', 'preto': 'black', 'azul': 'blue', 'roxo': 'purple', 'amarelo': 'yellow', 'laranja': 'orange', 'azul escuro': 'dark blue',
       'verde': 'green', 'cinza': 'grey', 'branco': 'white'}
 
-cor = input('Digite uma cor em portugês: ')
+# .lower() transforma tudo em letras minusculas.
+cor = input('Digite uma cor em portugês: ').lower()
 
 #O get() é um método usado para pegar o valor de uma dada chave em um dicionário se a chave estiver no dicionário,
 # caso ela não exista, o método retorna None ou o valor padrão passado por parâmetro.
